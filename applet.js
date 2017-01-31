2c0b7d2da7fdbca2f26ab4e8050510dbeb4b59cb
TeaTiMe08 <08teatime08@web.de>
2017-01-31 16:46:25 +0100

    commit

--- applet.js
+++ applet.js
@@ -19,7 +19,7 @@ MyApplet.prototype = {
     _init: function(orientation, panel_height, instance_id) {
         Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
 
-        this.set_applet_icon_name("force-exit");
+        this.set_applet_icon_name("icon");
         this.set_applet_tooltip(_("Tooltip"));
 
         this.settings = new Settings.AppletSettings(this, "lastfmview@randomchain.ddns.net", this.instance_id);
